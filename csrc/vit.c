c63a87f11b234ca808b1ee4ababc4066c5d2c4fc
SiddhantSKarki <your_github_email>
2025-01-07 04:52:44 -0500

    Initiated ViT.c

---
+++ csrc/vit.c
@@ -0,0 +1,13 @@
+// Copyright Siddhant S. Karki @ 2025
+
+// This Project follows the design patterns used by LLM.c
+// which can be found on https://github.com/karpathy/llm.c/tree/master.
+// In this project, we introduce you ViT.c, a low level image detection model
+// which reprocduces the results by the paper "An Image is worth 16x16 words:
+// Transformers for Image Recognition at Scale".
+// This project is being built to encourage the idea of using langauges like C,
+//  in a very constrained manner to achieve optimal minimum for a DNN faster
+// than the implementations done in python.
+
+
+
